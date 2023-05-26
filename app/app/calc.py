6126a15381758f072e69edad5a98e55488008101
mayank85 <mayank85@users.noreply.github.com>
2023-05-26 23:43:50 +0900

    new modified image with database

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Add X and Y and return result."""
+
+    return x + y
+
+def subtract(x, y):
+    """Subtract X from Y and return result"""
+
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """test adding two numbers together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract(self):
+        """Test the subtraction function"""
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, -5)
